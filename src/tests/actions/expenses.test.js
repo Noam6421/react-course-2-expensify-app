7040308dfbf2c123f9b6c98c515f226d2cc5e3e5
Noam <Noam6421@gmail.com>
2020-04-15 13:47:06 +0300

    Setup startEditExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
     removeExpense, 
     startRemoveExpense, 
     setExpenses, 
-    startSetExpenses 
+    startSetExpenses, 
+    startEditExpense
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
@@ -53,6 +54,24 @@ test('should setup edit expense action object', () => {
         id: '123abc',
         updates: { note: 'New note value' }
     });
+});
+
+test('should edit expense from firebase', (done) => {
+    const store = createMockStore({});
+    const id = expenses[0].id
+    const updates = { note: 'startEditExpense worked' };
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        });
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val().note).toBe(updates.note);
+        done()
+    })
 });
 
 test('should setup add expense action object with provided values', () => {
